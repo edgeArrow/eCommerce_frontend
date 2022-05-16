066e83e798164e6656d2559bb23ab78ff218dbf7
ntsyhani <tsyhanyuknazar@gmail.com>
2022-05-16 23:05:36 +0200

    Add constructor to product.ts

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,12 +1,32 @@
 export class Product {
-    sku!: string;
-    name!: string;
-    description!: string;
-    unitPrice!: number;
-    imageUrl!: string;
-    active!: boolean;
-    unitsInStock!: number;
-    dateCreated!: Date;
-    lastUpdated!: Date;
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+    constructor(sku: string,
+      name: string,
+      description: string,
+      unitPrice: number,
+      imageUr: string,
+      active: boolean,
+      unitsInStock: number,
+      dateCreated: Date,
+      lastUpdated: Date) {
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.imageUrl = imageUr;
+        this.active = active;
+        this.unitsInStock = unitsInStock;
+        this.dateCreated = dateCreated;
+        this.lastUpdated = lastUpdated;
+    }
 
 }
